53a65c177a7f81c59f5ae71d6f68b98cb3bac2a3
Daniyal Amjad <dadaniyal.amjad@yahoo.com>
2020-09-15 17:14:45 +0500

    setup woman mart

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const db = require('./src/db/mongoose');
+
+
+const port = 8080;
+
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log('app listening on port ' + port);
+});
