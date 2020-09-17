a41460fdb3262dcabba4d4e53ccad02987e8c4d9
Daniyal Amjad <dadaniyal.amjad@yahoo.com>
2020-09-17 12:00:00 +0500

    bugs solve

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = new express.Router();
 const categoryController = require("../controller/categoryController")
 
+/* Category Api's */
 router.post("/api/new-category", categoryController.categoryCreate)
 router.get("/api/category", categoryController.getCategory)
 router.patch("/api/update-category/:id", categoryController.updateCategory);
 router.patch("/api/update-delete/:id", categoryController.categoryDelete);
-
+/* Category Api's */
 module.exports = router
